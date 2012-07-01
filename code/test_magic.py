331b74a1c0cf693f6f17ea71abc38c0311990349
Antonio Cuni <anto.cuni@gmail.com>
2012-07-02 00:38:41 +0200

    more tests

--- code/test_magic.py
+++ code/test_magic.py
@@ -7,3 +7,18 @@ def test_rebind_globals():
     py.test.raises(NameError, "foo()")
     foo2 = magic.rebind_globals(foo, {'A': 42})
     assert foo2() == 42
+
+
+def test_extendabletype():
+    class A:
+        __metaclass__ = magic.extendabletype
+    class __extend__(A):
+        foo = 42
+    #
+    assert __extend__ is None
+    assert A.foo == 42
+    class B:
+        pass
+    class __extend__(A, B):
+        bar = 43
+    assert A.bar == B.bar == 43
